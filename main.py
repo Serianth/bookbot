b1f18cee5dbe1c506ad8ce724f82df4c94a4b586
Serianth <222801879+Serianth@users.noreply.github.com>
2025-07-27 15:30:27 +0100

    Finished main program

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 #get_book_text retrieves the book
 def get_book_text(book):
     with open(book) as f:
@@ -8,12 +12,12 @@ from stats import character_count
 from stats import sort_characters
 #main function - currently uses the above two functions in order to print out how many words are in the book.
 def main():
-    book_contents = get_book_text("books/frankenstein.txt")
+    book_contents = get_book_text(sys.argv[1])
     num_words = word_count(book_contents)
     num_letters = character_count(book_contents)
     character_list = sort_characters(num_letters)
     print (f"============ BOOKBOT ============")
-    print (f"Analyzing book found at books/frankenstein.txt...")
+    print (f"Analyzing book found at {sys.argv[1]}")
     print (f"----------- Word Count ----------")
     print (f"Found {num_words} total words")
     print (f"--------- Character Count -------")
